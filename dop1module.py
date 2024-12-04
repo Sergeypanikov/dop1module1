8e38caaedfb723435ecc0b3ff89473523cb10213
Sergeypanikov <svp1967@gmail.com>
2024-12-04 14:19:06 +0300

    дополнительное практическое задание к 1 модулю
    Возможно, можно было бы все упростить, но я не знаю пока как это сделать. Главное, результат на табло! (в консоли)

---
+++ dop1module.py
@@ -0,0 +1,46 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = sorted(list(students))
+print(students)
+a = grades[0]
+print(a)
+a_ = len(a)
+print(a_)
+a__ = sum(grades[0])
+print(a__)
+a___ = a__/a_
+print(a___)
+b = grades[1]
+print(b)
+b_ = len(b)
+print(b_)
+b__ = sum(grades[1])
+print(b__)
+b___ = b__/b_
+print(b___)
+j = grades[2]
+print(j)
+j_ = len(j)
+print(j_)
+j__ = sum(grades[2])
+print(j__)
+j___ = j__/j_
+print(j___)
+k = grades[3]
+print(k)
+k_ = len(k)
+print(k_)
+k__ = sum(grades[3])
+print(k__)
+k___ = k__/k_
+print(k___)
+s = grades[4]
+print(j)
+s_ = len(s)
+print(s_)
+s__ = sum(grades[4])
+print(s__)
+s___ = s__/s_
+print(s___)
+students = {'Aaron' : a___, 'Bilbo' : b___, 'Johnny' : j___, 'Khendrik' : k___, 'Steve' : s___}
+print(students)
